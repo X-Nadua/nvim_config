bf1e23f8588d911f1b659337e1b7ba58b4cb4114
Nadua <x.nadua.cirical@gmail.com>
2025-10-25 18:54:14 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1014,3 +1014,6 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+vim.opt.termguicolors = true
+vim.cmd("colorscheme <name>")

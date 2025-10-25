77aee638190ec7e68bedcdafe669a5f93d5d69db
Nadua <x.nadua.cirical@gmail.com>
2025-10-25 18:58:43 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1012,8 +1012,8 @@ require('lazy').setup({
   },
 })
 
+vim.opt.termguicolors = true
+vim.cmd("colorscheme Maroonish")
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-
-vim.opt.termguicolors = true
-vim.cmd("colorscheme <name>")
